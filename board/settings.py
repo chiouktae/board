f60f1b5af57f04fa5acdd90df77bbcb39fa6ae4c
chiouktae <cyt483566@gmail.com>
2020-08-11 16:41:36 +0900

    allowed hosts change

--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j-v8%fey7%3$0+ik9s^nl74z976a5%qrjv@&bu^9og5-!q)tip'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
